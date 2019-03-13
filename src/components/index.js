6717e3731776127bc9eb5ba7f46a044b2c14d9f3
Godfred Addai Boateng <addaiz.godfred@gmail.com>
2019-03-13 12:53:06 +0000

    Added Spinner component to index file

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import AddButton from './AddButton';
 import CommonInput from './CommonInput';
 import CustomButton from './CustomButton';
+import Spinner from './Spinner';
 
 export {
     AddButton,
     CommonInput,
-    CustomButton
+    CustomButton,
+    Spinner
 };
