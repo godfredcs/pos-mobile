5813ef8041ff74e4942b655a07f0680c1e6403ad
Godfred Addai Boateng <addaiz.godfred@gmail.com>
2019-03-13 12:52:24 +0000

    Created Spinner component

---
+++ src/components/Spinner.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { ActivityIndicator } from 'react-native';
+
+const Spinner = ({ size="small" }) => (
+    <ActivityIndicator size={ size } />
+)
+
+export default Spinner;
