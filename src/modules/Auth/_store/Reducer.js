18175a90efe62f2662f1385c44809acb3eaf93e7
Godfred Addai Boateng <addaiz.godfred@gmail.com>
2019-03-13 12:54:35 +0000

    Added other Auth states to reducer

--- src/modules/Auth/_store/Reducer.js
+++ src/modules/Auth/_store/Reducer.js
@@ -1,19 +1,58 @@
 import {
-    EMAIL_CHANGED, PASSWORD_CHANGED
+    FIRSTNAME_CHANGED, LASTNAME_CHANGED, EMAIL_CHANGED, PASSWORD_CHANGED, PASSWORD_CONFIRMATION_CHANGED, USER_PHOTO_CHANGED,
+    ATTEMPT_USER_LOGIN, USER_LOGIN_FAIL, USER_LOGIN_SUCCESS,
+    ATTEMPT_USER_REGISTER, USER_REGISTER_FAIL, USER_REGISTER_SUCCESS
 } from './Types';
 
 const INITIAL_STATE = {
+    user: null,
+    firstname: '',
+    lastname: '',
     email: '',
-    password: ''
+    password: '',
+    password_confirmation: '',
+    user_photo: '',
+    attempting_user_login: false,
+    attempting_user_register: false,
 };
 
 export default (state=INITIAL_STATE, action) => {
     switch (action.type) {
+        case FIRSTNAME_CHANGED:
+            return { ...state, firstname: action.payload };
+
+        case LASTNAME_CHANGED:
+            return { ...state, lastname: action.payload };
+
         case EMAIL_CHANGED:
             return { ...state, email: action.payload };
 
         case PASSWORD_CHANGED:
             return { ...state, password: action.payload };
+
+        case PASSWORD_CONFIRMATION_CHANGED:
+            return { ...state, password_confirmation: action.payload };
+
+        case USER_PHOTO_CHANGED:
+            return { ...state, user_photo: action.payload };
+
+        case ATTEMPT_USER_LOGIN:
+            return { ...state, attempting_user_login: true };
+
+        case USER_LOGIN_FAIL:
+            return { ...state, attempting_user_login: false, user_login_errors: action.payload };
+
+        case USER_LOGIN_SUCCESS:
+            return { ...state, attempting_user_login: false, user: action.payload };
+
+        case ATTEMPT_USER_REGISTER:
+            return { ...state, attempting_user_register: true };
+
+        case USER_REGISTER_FAIL:
+            return { ...state, attempting_user_register: false, user_register_errors: action.payload };
+
+        case USER_REGISTER_SUCCESS:
+            return { ...state, attempting_user_register: false, user: action.payload };
 
         default:
             return state;
