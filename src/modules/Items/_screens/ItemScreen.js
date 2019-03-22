80a4896b8130ed66f00497a1c83a3d766df3f3b4
Godfred Addai Boateng <addaiz.godfred@gmail.com>
2019-03-22 15:08:52 +0000

    Modified ItemScreen.js

--- src/modules/Items/_screens/ItemScreen.js
+++ src/modules/Items/_screens/ItemScreen.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
 import { connect } from 'react-redux';
 import { Styles } from '../../../globals';
 import { AddButton } from '../../../components';
 import { AddItemModal } from '../_components';
-import { getAllItems } from '../_store/Actions';
+import { getAllItems, selectItem } from '../_store/Actions';
 
 class ItemScreen extends Component {
     state = { showAddItemModal: false };
@@ -14,22 +14,54 @@ class ItemScreen extends Component {
     }
 
     /**
-     * Function for show modal for adding items.
+     * Function for show modal for adding items
+     *
+     * @return {void}
      */
     showAddItemModal = () => {
         this.setState(() => ({ showAddItemModal: true }));
     }
 
     /**
-     * Function for removing modal for adding items.
+     * Function for removing modal for adding items
+     *
+     * @return {void}
      */
     closeAddItemModal = () => {
         this.setState(() => ({ showAddItemModal: false }));
     }
 
+    /**
+     * Method for rendering the list of items
+     *
+     * @return {Array}
+     */
+    renderItems = () => {
+        return this.props.items.map(item => (
+            <TouchableOpacity key={ item.id } style={ styles.row } onPress={() => this.itemPress(item)}>
+                <Text>{ item.name }</Text>
+            </TouchableOpacity>
+        ))
+    }
+
+    /**
+     * Method for when an item is selected
+     *
+     * @param {Object} item
+     * @return {void}
+     */
+    itemPress = item => {
+        this.props.selectItem(item);
+        this.props.navigation.navigate('ItemViewScreen', { title: item.name });
+    }
+
     render() {
         return (
             <View style={ Styles.container }>
+
+                <View>
+                    { this.renderItems() }
+                </View>
 
                 <AddButton
                     onPress={ this.showAddItemModal }
@@ -43,9 +75,18 @@ class ItemScreen extends Component {
     }
 }
 
+const styles = StyleSheet.create({
+    row: {
+        paddingVertical: 10,
+        borderBottomWidth: 1,
+        paddingHorizontal: 20,
+        borderBottomColor: '#CCC'
+    }
+});
+
 const mapStateToProps = state => {
     const { items } = state.items;
     return { items };
 };
 
-export default connect(mapStateToProps, { getAllItems })(ItemScreen);
+export default connect(mapStateToProps, { getAllItems, selectItem })(ItemScreen);
