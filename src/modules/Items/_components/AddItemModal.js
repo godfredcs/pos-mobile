760fdde11e084ff1d50eac57974cefa95067e541
Godfred Addai Boateng <addaiz.godfred@gmail.com>
2019-03-23 12:57:50 +0000

    Modified AddItem modal

--- src/modules/Items/_components/AddItemModal.js
+++ src/modules/Items/_components/AddItemModal.js
@@ -1,55 +1,93 @@
-import React from 'react';
-import { Modal, View, Text, TouchableOpacity } from 'react-native';
+import React, { Component } from 'react';
+import { Modal, View, Text, TouchableOpacity, Alert } from 'react-native';
 import { connect } from 'react-redux';
 import { CommonInput } from '../../../components';
 import { Colors } from '../../../globals';
+import {
+	itemNameChanged, itemQuantityChanged, itemUnitPriceChanged, itemWholePriceChanged,
+	addItem, getAllItems
+} from '../_store/Actions';
 
-const AddItemModal = ({ visible, close }) => (
-	<Modal
-		transparent
-		visible={ visible }
-		animationType="slide"
-		onRequestClose={() => {}}>
+class AddItemModal extends Component {
+	/**
+	 * Method for adding item to database
+	 *
+	 * @return
+	 */
+	onAddItem = () => {
+		const data = {
+			name: this.props.item_name,
+			quantity: this.props.item_quantity,
+			unit_price: this.props.item_unit_price
+		};
 
-		<View style={ styles.container }>
-			<View style={ styles.card }>
-				<View style={{ padding: 20 }}>
-					<Text style={ styles.modalTitle }>ADD NEW ITEM</Text>
+		if (data.name && Number(data.quantity) && Number(data.unit_price)) {
+			return this.props.addItem(data, this.reloadAllItems);
+		}
+
+		return Alert.alert('Error', 'Please provide all the appropriate information');
+	}
+
+	reloadAllItems = () => {
+		this.props.getAllItems();
+		this.props.close();
+	}
+
+	render() {
+		return (
+			<Modal
+				transparent
+				visible={ this.props.visible }
+				animationType="slide"
+				onRequestClose={() => {}}>
+
+				<View style={ styles.container }>
+					<View style={ styles.card }>
+						<View style={{ padding: 20 }}>
+							<Text style={ styles.modalTitle }>ADD NEW ITEM</Text>
+						</View>
+
+						<View style={{ padding: 20 }}>
+							<CommonInput
+								label="Name"
+								value={ this.props.item_name }
+								onChangeText={ value => this.props.itemNameChanged(value) }
+							/>
+
+							<CommonInput
+								label="Quantity"
+								value={ this.props.item_quantity }
+								onChangeText={ value => this.props.itemQuantityChanged(value) }
+							/>
+
+							<CommonInput
+								label="Unit price"
+								value={ this.props.item_unit_price }
+								onChangeText={ value => this.props.itemUnitPriceChanged(value) }
+							/>
+						</View>
+
+						<View style={{ flexDirection: 'row', alignItems: 'center' }}>
+							<TouchableOpacity
+								onPress={ this.props.close }
+								style={[styles.buttons, { backgroundColor: Colors.danger, borderBottomLeftRadius: 5 }]}>
+
+								<Text style={ styles.buttonText }>CLOSE</Text>
+							</TouchableOpacity>
+
+							<TouchableOpacity
+								onPress={ this.onAddItem }
+								style={[styles.buttons, { backgroundColor: Colors.accent, borderBottomRightRadius: 5 }]}>
+
+								<Text style={ styles.buttonText }>ADD</Text>
+							</TouchableOpacity>
+						</View>
+					</View>
 				</View>
-
-				<View style={{ padding: 20 }}>
-                    <CommonInput
-                        label="Name" />
-
-					<CommonInput
-						label="Quantity" />
-
-                    <CommonInput
-                        label="Unit price" />
-
-                    <CommonInput
-                        label="Whole price" />
-				</View>
-
-				<View style={{ flexDirection: 'row', alignItems: 'center' }}>
-					<TouchableOpacity
-                        onPress={ close }
-                        style={[styles.buttons, { backgroundColor: Colors.danger, borderBottomLeftRadius: 5 }]}>
-
-						<Text style={ styles.buttonText }>CLOSE</Text>
-					</TouchableOpacity>
-
-					<TouchableOpacity
-                        onPress={ onAddItem }
-                        style={[styles.buttons, { backgroundColor: Colors.accent, borderBottomRightRadius: 5 }]}>
-
-						<Text style={ styles.buttonText }>ADD</Text>
-					</TouchableOpacity>
-				</View>
-			</View>
-		</View>
-	</Modal>
-);
+			</Modal>
+		);
+	}
+}
 
 const styles = {
 	container: {
@@ -86,4 +124,7 @@ const mapStateToProps = state => {
 	return { item_name, item_quantity, item_unit_price, item_whole_price };
 };
 
-export default connect(mapStateToProps, {  })(AddItemModal);
+export default connect(mapStateToProps, {
+	itemNameChanged, itemQuantityChanged, itemUnitPriceChanged, itemWholePriceChanged,
+	addItem, getAllItems
+})(AddItemModal);
